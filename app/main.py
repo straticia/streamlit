6fadb9c754f8415bd6447df713b399eeb9518e49
Terence0001 <111972075+Terence0001@users.noreply.github.com>
2023-12-29 01:02:09 +0400

    Update main.py
    L'application est relativement stable, maintenant place au déploiement

--- app/main.py
+++ app/main.py
@@ -26,34 +26,41 @@ def construct_prompt_for_summary(user_query:str, matches):
     prompt = f"{user_query}\n\nContexte:\n{context}"
     return prompt
 
-def clean_and_format_text(text:str):
-    # Supprime les sauts de ligne et les espaces inutiles
-    cleaned_text = ' '.join(text.split())
-    return cleaned_text
-
 
 
 #Informations de la page
 st.title("Straticia test")
 st.write("Application de recherche documentaire dans le droit (Limité aux marchés public)")
 
-query = st.text_input("Entrez votre requête de recherche:")
 
-if st.button("Démarrer l'Opération"):
-    my_bar = st.progress(5)
+
+# Créer un formulaire
+with st.form(key="search_form"):
+    # Champ de saisie de la requête
+    query = st.text_input("Entrez votre requête de recherche:")
+    
+    # Bouton pour démarrer l'opération de recherche
+    submit_button = st.form_submit_button(label="Démarrer l'Opération")
+
+
+# Fonction pour effectuer la recherche et afficher les résultats
+def effectuer_recherche():
+    my_bar = st.progress(0)
+    time.sleep(1)
+    my_bar.progress(20)
+
     # Effectuer la recherche avec Pinecone
     matches = search_docs(query, index)  # Assurez-vous que search_docs est définie et importée
-    my_bar.progress(20)
+    my_bar.progress(40)
+
     # Construire le prompt pour le modèle GPT-3
     prompt = construct_prompt_for_summary(query, matches)
-    my_bar.progress(35)
+    my_bar.progress(60)
     time.sleep(1)
-    my_bar.progress(45)
+    my_bar.progress(80)
 
     # Définir les paramètres de la requête OpenAI
     max_completion_tokens = 300
-    my_bar.progress(75)
-    # Exécuter la requête avec les paramètres ajustés
     response = openai.completions.create(
         model="gpt-3.5-turbo-instruct",
         prompt=prompt,
@@ -61,11 +68,13 @@ if st.button("Démarrer l'Opération"):
         temperature=0,
     )
 
-    time.sleep(1)
     my_bar.progress(100)
-
     time.sleep(1)
     my_bar.empty()
 
     st.success("Opération terminée !")
     st.write(response.choices[0].text)
+
+# Vérifier si le formulaire a été soumis
+if query:
+    effectuer_recherche()
